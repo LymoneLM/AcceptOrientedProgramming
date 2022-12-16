3ec6caef7feb1265b734e7c4e9bbb24a3729de72
Lymone <978462746@qq.com>
2022-12-16 15:27:10 +0800

    22.12.16

--- CUMT_PTA/Group2/7-8.cpp
+++ CUMT_PTA/Group2/7-8.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-    
+    char data[1001];
+    cin.getline(data,1001);
+    int len=strlen(data);
+    for(int i=0;i<len;i++)
+    {
+        if(data[i]=='6')
+        {
+            int temp=0;
+            while(data[(++temp)+i]=='6');--temp;
+            if(temp>=9)cout<<27;
+            else if(temp>=3)cout<<9;
+            else for(int i=0;i<=temp;i++)cout<<6;
+            i+=temp,temp=0;
+        }
+        else cout<<data[i];
+    }
     return 0;
 }
